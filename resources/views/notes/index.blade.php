edd3aacfd54a9d2958b3639a01759f38d250bac3
slusa <pawelslusarczyk00@gmail.com>
2019-03-02 18:37:04 +0100

    Added a temp modal for deleting note on main page

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -26,7 +26,7 @@
                     </div>
                     <p class="card-text pr-3 pl-3">{{ $note->content }}</p>
                     <div class="d-flex justify-content-start">
-                        <button type="button" class="btn p-3"><a href="{{ url('notes/destroy', $note->id) }}"><i class="fas fa-trash-alt note__top-icon"></i></a></button>
+                        <button type="button" class="btn p-3"><a href="{{ url('notes/destroy', $note->id) }}" data-toggle="modal" data-target="#deleteModal"><i class="fas fa-trash-alt note__top-icon"></i></a></button>
                         <button type="button" class="btn p-3"><a href="{{ url('notes', $note->id) }}"><i class="fas fa-marker note__top-icon"></i></a></button>
                         <button type="button" class="btn p-3"><a href="{{ url('notes/destroy', $note->id) }}"><i class="fas fa-trash-alt note__top-icon"></i></a></button>
                         <button type="button" class="btn p-3"><a href="{{ url('notes', $note->id) }}"><i class="fas fa-marker note__top-icon"></i></a></button>
@@ -37,6 +37,32 @@
             </div>
         </div>
     @endforeach
+
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title text-secondary" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body text-secondary">
+                  Czy na pewno chcesz usunąć notatkę <span class="text-primary">{{ $note->title }}</span> ?
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+                    {!! Form::open(['method' => 'DELETE', 'action' => ['NotesController@destroy', $note->id]]) !!}
+                        <button type="submit" class="btn btn-danger">Usuń</button>
+                    {!! Form::close() !!}
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
 </div>
 
 @endsection
+
