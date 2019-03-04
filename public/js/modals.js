f0a21729f10c9b1d2f202902b24d196841d672e6
slusa <pawelslusarczyk00@gmail.com>
2019-03-04 19:41:11 +0100

    Added focus on title input form

--- public/js/modals.js
+++ public/js/modals.js
@@ -1,13 +1,7 @@
-console.log('kurczaki');
-
 $("body").on("click",".deleteModal",function(){
     var id = $(this).data('id');
-    console.log(id);
     var title = $(this).data('title');
-    console.log('title');
     $("#deleteModal").find(".modalTitle").text(title);
     var form = $("#deleteModal").find("form");
-    console.log(form[0].action);
     form.attr('action', $(this).data('delete-link'));
-    console.log(form[0].action);
 });
--- resources/views/notes/form.blade.php
+++ resources/views/notes/form.blade.php
@@ -1,6 +1,6 @@
 {!! Form::hidden('user_id', auth()->user()->id) !!}
 <div class="form-group">
-    {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Tytuł notatki']) !!}
+    {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Tytuł notatki', 'autofocus'=>'autofocus']) !!}
 </div>
 <div class="form-group">
     {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Treść notatki']) !!}
