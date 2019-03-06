f378c7e64125373a711a70e3075ff0c9f1290e6b
slusa <pawelslusarczyk00@gmail.com>
2019-03-06 21:59:15 +0100

    Changed timezone to Warsaw in configs app

--- database/migrations/2019_03_02_202400_create_notes_table.php
+++ database/migrations/2019_03_02_202400_create_notes_table.php
@@ -18,10 +18,9 @@ class CreateNotesTable extends Migration
             $table->bigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('title');
-            $table->integer('priority')->default(0);;
+            $table->integer('priority')->default(0);
             $table->text('content');
             $table->timestamps();
-
         });
     }
 
