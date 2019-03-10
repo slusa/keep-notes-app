d1a1afa78acb643b6c6b5a86ad15d2190bce9d27
slusa <pawelslusarczyk00@gmail.com>
2019-03-10 09:59:09 +0100

    Added polish labels in logging forms

--- resources/views/notes/form.blade.php
+++ resources/views/notes/form.blade.php
@@ -1,9 +1,9 @@
     {!! Form::hidden('user_id', auth()->user()->id) !!}
 <div class="form-group">
-    {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Tytuł notatki', 'autofocus'=>'autofocus']) !!}
+    {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Tytuł notatki', 'autofocus'=>'autofocus', 'required' => 'required']) !!}
 </div>
 <div class="form-group">
-    {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Treść notatki']) !!}
+    {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Treść notatki', 'required' => 'required']) !!}
 </div>
 
 @if (Session::has('note_editing'))
